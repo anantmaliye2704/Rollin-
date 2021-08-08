9f055d405362895e2671e7fecf2037c8ba81a0be
Anant Maliye <65802411+anantmaliye2704@users.noreply.github.com>
2021-08-08 18:59:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Dice 1
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/"+ randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// Dice 2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource = "images/"+ randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource);
+
